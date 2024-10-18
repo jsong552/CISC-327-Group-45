29f9b12c58135b144a157f8f99dc668a62c46460
AdwaitSri <studyuse1702@gmail.com>
2024-10-18 14:50:30 -0400

    UNDOING JAMES ZAZA

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './output.css';
 import App from './App';
 import Sidebar from './components/sidebar.tsx';
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <BrowserRouter>
     <div>
     <App />
-    <Sidebar/>
+    {/* <Sidebar/> */}
     </div>
+    </BrowserRouter>
 );
