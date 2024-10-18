522d29c0246f3749ecf768e4eba20cf90cfeef5a
jsong552 <james.song017@gmail.com>
2024-10-18 17:09:45 -0400

    dashboard main part done

---
+++ src/components/DashboardComponent.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export const DashboardComponent = ({ image, borderColor, color, title, subtitle, desc, link }) => {
+    const outerDivClassName = `h-64 border-2 border-[${borderColor}] flex flex-col items-center justify-between`;
+    
+    return (
+        <div className={outerDivClassName}>
+            <div className="flex flex-col items-center justify-around h-40 pt-8">
+                <img 
+                    src={image}
+                    alt="add medicine icon"
+                    className="w-[50px] h-[50px]"
+                />
+                <p className="font-bold text-3xl">{title}</p>
+                <p className="font-semibold text-xl">{subtitle}</p>
+            </div>
+            <div className={`h-12 bg-[${color}] border-t-2 border-t-[${borderColor}] w-full`}>
+                <div className="mx-auto w-2/3 h-full flex flex-col justify-around">
+                    <div className="w-fit mx-auto">     
+                        <div className="flex flex-row w-full gap-2">
+                            <p>{desc}</p>
+                            <img 
+                                src="/arrowIcon.svg"
+                                alt="arrow icon"
+                                className="w-[15px] h-[15px] relative top-[5px]"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+// #f2e9ac
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,14 +1,63 @@
 import React from 'react'
+import { DashboardComponent } from '../components/DashboardComponent.tsx'
+import { DashboardComponentTwo } from '../components/DashboardComponentTwo.tsx'
 
 export const Dashboard = () => {
     return (
-        <div className="bg-[#edf1f5] w-screen h-screen flex flex-row">
-            {/*  enter side bar here */}
-            <div className="bg-[#121220] w-[25%] h-screen" />
-            <div className="">
-                <p className="text-2xl">Dashboard</p>
-                <p className="text-xl">Overview of the pharmacy information</p>
+        <>
+            <div className="bg-[#edf1f5] w-screen">
+                <div className="p-12 border">
+                    <p className="text-4xl font-semibold">Dashboard</p>
+                    <p className="text-xl mt-2">Overview of the pharmacy information</p>
+                    <div className="gap-4 grid grid-cols-3 mx-6 mt-12">
+                        <DashboardComponent 
+                            borderColor={"#59c39c"} 
+                            image="/addMedicinesIcon.svg" 
+                            title="Add Medicines"
+                            subtitle="To Inventory"
+                            desc="Add Medicines"
+                            color="#a6dbcb"
+                            link=""
+                        />
+                        <DashboardComponent 
+                            borderColor={"#fdd70b"} 
+                            image="/viewSalesIcon.svg"
+                            title="View Sales"
+                            subtitle="of the Year"
+                            desc="View Detailed Report"
+                            color="#f2e9ac"
+                            link=""
+                        />
+                        <DashboardComponent 
+                            borderColor={"#36b8f5"} 
+                            image="/medicinesAvailableIcon.svg"
+                            title="298"
+                            subtitle="Medicines Available"
+                            desc="View Inventory"
+                            color="#a7dcf5"
+                            link=""
+                        />
+                    </div>
+                </div>
             </div>
-        </div>
+            <div className="px-8 grid grid-cols-2 gap-8 mt-32 mb-32">
+                <DashboardComponentTwo 
+                    no1="298"
+                    no2="24"
+                    title="Inventory"
+                    subtitle="Go to Configuration"
+                    text1="Total no. of Medicines"
+                    text2="Medicine Groups"
+                />
+                <DashboardComponentTwo 
+                    no1="70,856"
+                    no2="5,288"
+                    title="Quick Report"
+                    subtitle="October 2024"
+                    text1="Qty. of Medicines Sold"
+                    text2="Invoices Generated"
+                />
+            </div>
+        </>
     )
 }
